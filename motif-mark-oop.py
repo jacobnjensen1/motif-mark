7d1c98ff5f41a8230ceefd73c5f7c8438cc9c075
Jacob Jensen <jnjensen88@gmail.com>
2023-02-23 13:16:57 -0800

    fix scaling, add key

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -2,6 +2,7 @@
 import argparse
 import re
 import math
+import cairo
 
 def get_args():
   parser = argparse.ArgumentParser(description="Finds binding motifs around an exon and displays them in a picture.")
@@ -34,7 +35,7 @@ class MotifSearcher:
 
   def findMotifs(self, searchSeq):
     matches = re.finditer(self.motifSearch, searchSeq.seq, re.IGNORECASE)
-    print([m.start() for m in matches])
+    return [m.start() for m in matches]
 
 class Seq:
   def __init__(self, header, seq):
@@ -63,10 +64,10 @@ class Seq:
       exonEnd = min(self.seq[exonStart:].index("t"), exonEnd)
     exonEnd += exonStart
 
-    return (0, exonStart, exonEnd, len(self.seq))
+    return [0, exonStart, exonEnd, len(self.seq)]
 
 def getHeadersAndSeqs(fileHandle):
-  allSeqs = []
+  allSeqs = [] #[Seq, Seq, ...]
   header = ""
   seq = ""
   for line in fileHandle:
@@ -84,8 +85,109 @@ def getHeadersAndSeqs(fileHandle):
         allSeqs.append(Seq(header, seq))
   return allSeqs
 
+def scalePositions(positionDict, boundaries, width, margin, bpWidth):
+  #TODO: change scaling to all bps the same
+  
+  scaledBoundaries = [(entry * bpWidth) + margin for entry in boundaries]
+
+  # scaledPostions = {} #motif:[(start,end), ...]
+  # for motif, positions in positionDict.items():
+  #   tempPositions = [((width - margin) - margin) * ((entry - boundaries[0]) / (boundaries[-1] - boundaries[0])) + margin for entry in positions]
+  #   scaledMotifLen = ((width - margin) - margin) * ((len(motif) - boundaries[0]) / (boundaries[-1] - boundaries[0])) + margin
+  #   scaledPostions[motif] = [(tempPositions[i], tempPositions[i] + scaledMotifLen) for i in range(len(tempPositions))]
+
+  scaledPostions = {} #motif:[(start,end), ...]
+  for motif, positions in positionDict.items():
+    tempPositions = [(entry * bpWidth) + margin for entry in positions]
+    scaledPostions[motif] = [(tempPositions[i], tempPositions[i] + (len(motif) * bpWidth)) for i in range(len(tempPositions))]
+  
+  print(scaledBoundaries)
+  return scaledBoundaries, scaledPostions
+
+def draw(seqs, searchers, fastaName):  
+  colorPalette = [(213/255, 94/255, 0),
+                  (0, 114/255, 178/255),
+                  (240/255, 228/255, 66/255),
+                  (0, 158/255, 115/255),
+                  (204/255, 121/255, 167/255)]
+
+  margin = 20
+  width = 1000
+  heightPerGroup = 100
+  legendHeight = heightPerGroup
+  height = (heightPerGroup * len(seqs)) + legendHeight
+  bpWidth = (width - (margin * 2)) / max([len(seq.seq) for seq in seqs])
+  
+  fileBaseName = ".".join(fastaName.split(".")[:-1])
+  surface = cairo.SVGSurface(f"{fileBaseName}.svg", width, height)
+  context = cairo.Context(surface)
+  context.select_font_face(
+        "Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+  
+  context.set_source_rgb(1,1,1)
+  context.rectangle(0,0,width,height)
+  context.fill()
+
+  for i, searcher in enumerate(searchers):
+    yCenter = legendHeight / 2
+    xStart = margin + ((width - (margin * 2)) * (i / len(searchers)))
+
+    print(xStart)
+    
+    context.move_to(xStart, yCenter)
+    context.set_line_width(heightPerGroup / 2)
+    color = colorPalette[i]
+    context.set_source_rgba(color[0], color[1], color[2], 1)
+    context.line_to(xStart + heightPerGroup / 2, yCenter)
+    context.stroke()
+
+    context.set_font_size(heightPerGroup / 4)
+    context.move_to(xStart + heightPerGroup/2 + margin / 2, legendHeight / 1.5)
+    context.show_text(searcher.motifSeq)
+
+  for i, seq in enumerate(seqs):
+    boundaries = seq.getBoundaries()
+    print(boundaries)
+
+    positionDict = {}
+    for searcher in searchers:
+      positionDict[searcher.motifSeq] = searcher.findMotifs(seq)
+    boundaries, positionDict = scalePositions(positionDict, boundaries, width, margin, bpWidth)  #positionDict values are [(start, end),...], but scaled for printing
+    print((boundaries[2] - boundaries[1]) / boundaries[3])
+
+    yCenter = ((i + 1) * heightPerGroup) - (heightPerGroup / 2) + legendHeight
+    context.set_source_rgba(0, 0, 0, 1)
+    context.set_font_size(heightPerGroup / 3)
+    
+    context.move_to(margin, yCenter - heightPerGroup / 6)
+    context.show_text(seq.header.split(" ")[0])
+    
+    #draw line for entire sequence
+    context.move_to(boundaries[0], yCenter)
+    context.set_line_width(1)
+    context.line_to(boundaries[3], yCenter)
+    context.stroke()
+    
+    #draw really thick line for exon (rectangles are boring)
+    context.move_to(boundaries[1], yCenter)
+    context.set_line_width(heightPerGroup / 2)
+    context.line_to(boundaries[2], yCenter)
+    context.stroke()
+
+    #draw motifs
+    context.set_line_width(heightPerGroup/4)
+    for j, positions in enumerate(positionDict.values()):
+      color = colorPalette[j]
+      context.set_source_rgba(color[0], color[1], color[2], .75)
+      for positionGroup in positions:
+        context.move_to(positionGroup[0], yCenter)
+        context.line_to(positionGroup[1], yCenter)
+        context.stroke()
+  
+  surface.write_to_png(f"{fileBaseName}.png")
+
 def main():
-  args=get_args()
+  args = get_args()
 
   searchers = []
   with open(args.motifFile) as inFileMotif:
@@ -95,12 +197,8 @@ def main():
   with open(args.fasta) as inFileFasta:
     seqs = getHeadersAndSeqs(inFileFasta)
 
-  for seq in seqs:
-    print(seq.header)
-    for searcher in searchers:
-      print(searcher.motifSeq)
-      searcher.findMotifs(seq)
-    print("")
+  draw(seqs, searchers, args.fasta)
+  
 
 if __name__ == "__main__":
   main()
